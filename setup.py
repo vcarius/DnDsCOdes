960803f814f8e2f41614e8cb8ce6cedd324763c8
VINICIUS CARIUS DE SOUZA <35878693+vcarius@users.noreply.github.com>
2026-05-06 18:46:58 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='DnDsCODes',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[],  # Liste as dependências aqui, se houver
-)
